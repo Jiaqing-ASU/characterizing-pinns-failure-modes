08e7bee414c6d0690e2e07d8ed33235675f72fa7
Jiaqing Chen <jchen501@asu.edu>
2023-09-12 15:41:12 -0700

    train more models

--- pbc_examples/run_main_pbc.sh
+++ pbc_examples/run_main_pbc.sh
@@ -1,10 +1,18 @@
-# python3 main_pbc.py --beta=1.0 --visualiz=True --save_model=True
-# python3 main_pbc.py --beta=5.0 --visualiz=True --save_model=True
-# python3 main_pbc.py --beta=10.0 --visualiz=True --save_model=True
-# python3 main_pbc.py --beta=20.0 --visualiz=True --save_model=True
-# python3 main_pbc.py --beta=30.0 --visualiz=True --save_model=True
-# python3 main_pbc.py --beta=40.0 --visualiz=True --save_model=True
-# python3 main_pbc.py --beta=50.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=1.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=5.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=10.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=15.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=20.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=25.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=30.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=35.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=40.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=45.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=50.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=55.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=60.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=65.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=70.0 --visualiz=True --save_model=True
 
 # python3 main_pbc.py --system=reaction --rho=1.0 --visualiz=True --save_model=True
 # python3 main_pbc.py --system=reaction --rho=2.0 --visualiz=True --save_model=True
@@ -26,10 +34,10 @@
 # python3 main_pbc.py --seed=2023 --beta=1.0 --visualiz=True --save_model=True
 # python3 main_pbc.py --seed=2023 --beta=50.0 --visualiz=True --save_model=True
 
-python3 main_pbc.py --system=diffusion --nu=2.0 --visualiz=True --save_model=True
-python3 main_pbc.py --system=diffusion --nu=3.0 --visualiz=True --save_model=True
-python3 main_pbc.py --system=diffusion --nu=4.0 --visualiz=True --save_model=True
-python3 main_pbc.py --system=diffusion --nu=5.0 --visualiz=True --save_model=True
-python3 main_pbc.py --system=diffusion --nu=6.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --system=diffusion --nu=2.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --system=diffusion --nu=3.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --system=diffusion --nu=4.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --system=diffusion --nu=5.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --system=diffusion --nu=6.0 --visualiz=True --save_model=True
 
 echo "Finished all runs"
