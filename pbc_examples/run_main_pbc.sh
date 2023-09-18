6aece6c193f2e37093a130c18344a2639c5ee15a
Jiaqing Chen <jchen501@asu.edu>
2023-09-18 16:51:05 -0700

    add models with small beta value

--- pbc_examples/run_main_pbc.sh
+++ pbc_examples/run_main_pbc.sh
@@ -1,18 +1,29 @@
 python3 main_pbc.py --beta=1.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=2.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=3.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=4.0 --visualiz=True --save_model=True
 python3 main_pbc.py --beta=5.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=6.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=7.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=8.0 --visualiz=True --save_model=True
+python3 main_pbc.py --beta=9.0 --visualiz=True --save_model=True
 python3 main_pbc.py --beta=10.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=15.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=20.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=25.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=30.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=35.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=40.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=45.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=50.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=55.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=60.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=65.0 --visualiz=True --save_model=True
-python3 main_pbc.py --beta=70.0 --visualiz=True --save_model=True
+
+# python3 main_pbc.py --beta=1.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=5.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=10.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=15.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=20.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=25.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=30.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=35.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=40.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=45.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=50.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=55.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=60.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=65.0 --visualiz=True --save_model=True
+# python3 main_pbc.py --beta=70.0 --visualiz=True --save_model=True
 
 # python3 main_pbc.py --system=reaction --rho=1.0 --visualiz=True --save_model=True
 # python3 main_pbc.py --system=reaction --rho=2.0 --visualiz=True --save_model=True
